507d780aede7d192e4172249196eff58e6cbaa91
Qkyrie <quintendeswaef@proton.me>
2022-12-27 17:13:18 +0100

    initial commit of the infra

---
+++ terraform/networks.tf
@@ -0,0 +1,84 @@
+variable "networks" {
+  description = "networks that have their own service"
+  type        = list(string)
+  default     = [
+    "arbitrum", "avalanche", "binance", "ethereum", "fantom", "optimism", "polygon"
+  ]
+}
+
+resource "kubernetes_deployment" "defitrack-networks" {
+  count = length(var.networks)
+  metadata {
+    name   = "defitrack-${var.networks[count.index]}"
+    labels = {
+      app : "defitrack-${var.networks[count.index]}"
+    }
+  }
+  spec {
+    replicas = "1"
+    selector {
+      match_labels = {
+        app : "defitrack-${var.networks[count.index]}"
+      }
+    }
+    strategy {
+      type = "RollingUpdate"
+    }
+    template {
+      metadata {
+        labels = {
+          app : "defitrack-${var.networks[count.index]}"
+        }
+      }
+      spec {
+        volume {
+          name = "config-volume"
+          config_map {
+            name = "defitrack-${var.networks[count.index]}"
+          }
+        }
+        container {
+          image             = "${var.base-image}:${var.networks[count.index]}-production"
+          name              = "defitrack-${var.networks[count.index]}"
+          image_pull_policy = "Always"
+          volume_mount {
+            mount_path = "/config"
+            name       = "config-volume"
+          }
+          port {
+            container_port = 8080
+          }
+          readiness_probe {
+            http_get {
+              path = "/actuator/health"
+              port = 8080
+            }
+            initial_delay_seconds = 20
+            period_seconds        = 10
+            timeout_seconds       = 2
+            failure_threshold     = 1
+            success_threshold     = 1
+          }
+          liveness_probe {
+            http_get {
+              path = "/actuator/health"
+              port = 8080
+            }
+            initial_delay_seconds = 25
+            period_seconds        = 20
+            timeout_seconds       = 2
+            failure_threshold     = 1
+          }
+        }
+        image_pull_secrets {
+          name = "personal-docker-registry"
+        }
+        toleration {
+          key      = "node-role.kubernetes.io/master"
+          effect   = "NoSchedule"
+          operator = "Exists"
+        }
+      }
+    }
+  }
+}
---
+++ terraform/protocols.tf
@@ -0,0 +1,83 @@
+variable "protocols" {
+  description = "Protocols that have their own service"
+  type        = list(string)
+  default     = [
+    "olympusdao", "aave", "adamant", "apeswap", "aelin", "beefy", "compound", "balancer", "bancor", "beethovenx",
+    "dfyn", "dmm", "hop", "idex", "iron-bank", "kyberswap", "maplefinance", "mstable", "quickswap", "spirit", "spooky",
+    "stargate", "sushiswap", "uniswap", "polycat", "convex", "curve", "dinoswap", "ribbon", "set", "wepiggy",
+    "makerdao", "polygon-protocol"
+
+  ]
+}
+
+variable "base-image" {
+  default = "Base Image to pull from"
+  type    = string
+}
+
+resource "kubernetes_deployment" "defitrack-protocols" {
+  count = length(var.protocols)
+  metadata {
+    name   = "defitrack-${var.protocols[count.index]}"
+    labels = {
+      app : "defitrack-${var.protocols[count.index]}"
+    }
+  }
+  spec {
+    replicas = "1"
+    selector {
+      match_labels = {
+        app : "defitrack-${var.protocols[count.index]}"
+      }
+    }
+    strategy {
+      type = "RollingUpdate"
+    }
+    template {
+      metadata {
+        labels = {
+          app : "defitrack-${var.protocols[count.index]}"
+        }
+      }
+      spec {
+        container {
+          image             = "${var.base-image}:${var.protocols[count.index]}-production"
+          name              = "defitrack-${var.protocols[count.index]}"
+          image_pull_policy = "Always"
+          port {
+            container_port = 8080
+          }
+          readiness_probe {
+            http_get {
+              path = "/actuator/health"
+              port = 8080
+            }
+            initial_delay_seconds = 20
+            period_seconds        = 10
+            timeout_seconds       = 2
+            failure_threshold     = 1
+            success_threshold     = 1
+          }
+          liveness_probe {
+            http_get {
+              path = "/actuator/health"
+              port = 8080
+            }
+            initial_delay_seconds = 25
+            period_seconds        = 20
+            timeout_seconds       = 2
+            failure_threshold     = 1
+          }
+        }
+        image_pull_secrets {
+          name = "personal-docker-registry"
+        }
+        toleration {
+          key      = "node-role.kubernetes.io/master"
+          effect   = "NoSchedule"
+          operator = "Exists"
+        }
+      }
+    }
+  }
+}
