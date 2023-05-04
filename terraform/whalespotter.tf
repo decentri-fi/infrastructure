099f0bccfa311b40b4ff5536d9c51724cf24270f
Qkyrie <quintendeswaef@gmail.com>
2023-05-04 16:44:42 +0200

    move apps

--- terraform/whalespotter.tf
+++ terraform/whalespotter.tf
@@ -21,6 +21,27 @@ resource "kubernetes_service" "whalespotter-web" {
   }
 }
 
+resource "kubernetes_service" "whalespotter-suggestion-engine" {
+
+  metadata {
+    name      = "whalespotter-suggestion-engine"
+  }
+
+  spec {
+    selector = {
+      app = "whalespotter-suggestion-engine"
+    }
+
+    port {
+      name = "http"
+      port = 8080
+      target_port = 8080
+      protocol = "TCP"
+    }
+
+    type = "NodePort"
+  }
+}
 
 resource "kubernetes_service" "whalespotter-runner" {
 
@@ -75,6 +96,82 @@ resource "kubernetes_deployment" "whalespotter-web" {
         container {
           image             = "${var.base-image}:whalespotter-web-production"
           name              = "whalespotter-web"
+          image_pull_policy = "Always"
+          volume_mount {
+            mount_path = "/application/config"
+            name       = "config-volume"
+          }
+          port {
+            container_port = 8080
+          }
+          readiness_probe {
+            http_get {
+              path = "/actuator/health"
+              port = 8080
+            }
+            initial_delay_seconds = 20
+            period_seconds        = 10
+            timeout_seconds       = 2
+            failure_threshold     = 1
+            success_threshold     = 1
+          }
+          liveness_probe {
+            http_get {
+              path = "/actuator/health"
+              port = 8080
+            }
+            initial_delay_seconds = 25
+            period_seconds        = 20
+            timeout_seconds       = 2
+            failure_threshold     = 1
+          }
+        }
+        image_pull_secrets {
+          name = "personal-docker-registry"
+        }
+        toleration {
+          key      = "node-role.kubernetes.io/master"
+          effect   = "NoSchedule"
+          operator = "Exists"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "whalespotter-suggestion-engine" {
+  metadata {
+    name   = "whalespotter-suggestion-engine"
+    labels = {
+      app : "whalespotter-suggestion-engine"
+    }
+  }
+  spec {
+    replicas = "1"
+    selector {
+      match_labels = {
+        app : "whalespotter-suggestion-engine"
+      }
+    }
+    strategy {
+      type = "RollingUpdate"
+    }
+    template {
+      metadata {
+        labels = {
+          app : "whalespotter-suggestion-engine"
+        }
+      }
+      spec {
+        volume {
+          name = "config-volume"
+          config_map {
+            name = "whalespotter-web"
+          }
+        }
+        container {
+          image             = "${var.base-image}:whalespotter-suggestion-engine-production"
+          name              = "whalespotter-suggestion-engine"
           image_pull_policy = "Always"
           volume_mount {
             mount_path = "/application/config"
